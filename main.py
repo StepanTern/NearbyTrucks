e99bdb221dcd9e0622cda697e059ce6ed48edb72
vlad <master.redstone@yandex.ru>
2024-04-19 22:41:45 +0300

    tabs

--- main.py
+++ main.py
@@ -2,18 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-tabs = {
-    'index': 'Главная',
-    'favourite': 'Избранное',
-    'profile': 'Профиль',
-    'admin': 'Админка',
-}
-
 
 @app.route('/')
 @app.route('/index')
-def index():
-    return render_template('index.html', tabs=tabs)
+def home():
+    return render_template('index.html')
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/orders')
+def orders():
+    return render_template('orders.html')
+
+
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+
+@app.route('/customers')
+def customers():
+    return render_template('customers.html')
 
 
 if __name__ == '__main__':
